2d85b171b9846781d1b8a7c5a53ebf4408ab1634
shreyasnadig <shreyasnadig98@gmail.com>
2019-02-05 11:13:25 +0530

    updates

--- tst.py
+++ tst.py
@@ -1,3 +1,17 @@
-print("Helo World!")
+def add(a,b):
+    return a+b
+
+def compare (a,b,c):
+    if a<b and a<c:
+        return a
+    elif b<c:
+        return b
+    else:
+        return c
+
+
+print(add(2,67))
+print(compare(6,1,9))
+
 
 
