d5dff874d9512cf95516498f4f8683b4825ae637
shreyasnadig <42885687+shreyasnadig@users.noreply.github.com>
2019-02-08 21:50:59 +0530

    Update frac_modules.py

--- frac_modules.py
+++ frac_modules.py
@@ -1,12 +1,9 @@
 from fractions import Fraction
 
-<<<<<<< HEAD
-print(Fraction(1,2))
-=======
+
 def add_frac():
     x = Fraction(1,2)
     y = Fraction(2,3)
     print( x + y )
    
 add_frac()
->>>>>>> ae442e367385d82ed668f781dd49131c6a6897c1
