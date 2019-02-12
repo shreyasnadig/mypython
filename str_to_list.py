32b3dfa6b0a30ff4cd5b5a927e9617c8adfdf15b
shreyasnadig <42885687+shreyasnadig@users.noreply.github.com>
2019-02-12 20:36:44 +0530

    Update str_to_list.py

--- str_to_list.py
+++ str_to_list.py
@@ -1,11 +1,11 @@
 def str_to_list():
-	str = "a+b,c+d,e+f,hello+world"
+	str = "a=b,c=d,e=f,hello=world"
 	print(str)
 	ll = str.split(',')
 	final_list = []
 	for i in range(len(ll)):
 		s2= ''.join(ll[i])
-		ll2 = s2.split('+')
+		ll2 = s2.split('=')
 		final_list.append(tuple(ll2))
 	print ( final_list )
 
